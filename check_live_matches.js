50dff757ebe192cd33bac908dd2f8b23f11b5b17
Karthik Kondagurla <krtk0024@gmail.com>
2026-04-06 19:31:02 +0530

    chore: cleanup temporary test scripts

--- check_live_matches.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import Redis from 'ioredis';
-dotenv.config();
-const redis = new Redis(process.env.REDIS_URL);
-redis.get('live_matches').then(res => {
-  console.log(res);
-  process.exit(0);
-});
